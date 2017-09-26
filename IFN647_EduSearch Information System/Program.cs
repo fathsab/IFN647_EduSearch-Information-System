9ede0c594a40a861781031ab4ecc68551e23418b
Fathia Sabrina <n9605517@qut.edu.au>
2017-09-26 15:45:10 +1000

    separating query

--- IFN647_EduSearch Information System/Program.cs
+++ IFN647_EduSearch Information System/Program.cs
@@ -14,6 +14,19 @@ namespace IFN647_EduSearch_Information_System
         [STAThread]
         static void Main()
         {
+            string path = @"C:\temp\cran_information_needs.txt";
+
+            // SOLUTION 1
+            System.IO.TextReader reader = new System.IO.StreamReader(path);
+            string text = reader.ReadToEnd();
+            Console.WriteLine(text);
+            reader.Close();
+
+            //separating query
+            String[] substrings = text.Split(new char[] { '.', 'I', 'D' });
+            foreach (var substring in substrings)
+                Console.WriteLine(substring);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
