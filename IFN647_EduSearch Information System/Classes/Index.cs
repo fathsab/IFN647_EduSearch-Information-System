c9c73d78ea5d245ca8d1743adc0bd1d006694724
Yasiru Dassanayaka Liyanage <n9672141@qut.edu.au>
2017-09-26 15:21:03 +1000

    Add project files.

---
+++ IFN647_EduSearch Information System/Classes/Index.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFN647_EduSearch_Information_System.Classes
+{
+    class Index
+    {
+    }
+}
---
+++ IFN647_EduSearch Information System/UI/IndexForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IFN647_EduSearch_Information_System.UI
+{
+    public partial class IndexForm : Form
+    {
+        public IndexForm()
+        {
+            InitializeComponent();
+        }
+
+        private void IndexForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
